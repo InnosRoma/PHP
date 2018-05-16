6d0e536d25403aaa6a3df7a8626c0d7687c21fea
Roma <innosroma@mail.ru>
2018-05-16 19:32:48 +0300

    1605

--- Magna.js
+++ Magna.js
@@ -17,6 +17,7 @@
 //   });
 // console.log($("td>a"));
 
+
 $(document).on("click", "td a.delete", function(event) {
     event.preventDefault(); 
     var client_id = $(this).attr("data-id");
@@ -29,7 +30,35 @@ $(document).on("click", "td a.delete", function(event) {
     });
 });
 
-$(document).on("click", "td a.edit", function(event) {
+$(document).on("click", "td a.view", function(event) {
+    //event.preventDefault();
+    var client_id = $(this).attr("data-id");
+    var server_url = "http://localhost:81/PHP/services.php?q=user/view&id=" + client_id;
+    console.log(client_id);
+    $.getJSON(server_url, function(){
+        console.log( "Success" );
+    }).done(function(data){
+        if(data.active == 1 ){
+            var status = "Active";
+        } else status = "Non Active";
+        console.log(data);
+        var html ="";
+        html+="<div align=\"center\" style=\"font-size:25px\">" + data.first_name + " " + data.last_name + "</div>";
+        html+="<div align=\"left\" style=\"font-size:20px\">" + "Phone: " + data.phone + "</div>";
+        html+="<div align=\"left\" style=\"font-size:20px\">" + "Age: " + data.age + "</div>";
+        html+="<div align=\"left\" style=\"font-size:20px\">" + "Status: " + status + "</div>";
+        html+="<div class=\"interests\">Interests:<ul type = \"disc\">";
+        for(var i = 0; i < (data.interests.length); i++){
+        html+='<li align="left" style="font-size:20px">' + data.interests[i].description + "</li>";
+        }
+        html+="</ul></div>";
+        console.log(html);
+        console.log($('div.content'));
+        $('div.content').html(html);
+        });
+  });
+
+  $(document).on("click", "td a.edit", function(event) {
     //event.preventDefault();
     var client_id = $(this).attr("data-id");
     var server_url = "http://localhost:81/PHP/services.php?q=user/view&id=" + client_id;
@@ -38,18 +67,33 @@ $(document).on("click", "td a.edit", function(event) {
         console.log( "Success" );
     }).done(function(data){
         console.log(data);
+        if(data.active == 1 ){
+            var status = "Active";
+        } else status = "Non Active";
         var html ="";
-        html+="<div align=\"center\" style=\"font-size:25px\">" + data.first_name + " " + data.last_name + "</div>";
-        html+="<div align=\"left\" style=\"font-size:20px\">" + "Phone: " + data.phone + "</div>";
-        html+="<div align=\"left\" style=\"font-size:20px\">" + "Age: " + data.age + "</div>";
-        html+="<div align=\"left\" style=\"font-size:20px\">" + "Active: " + data.active + "</div>";
-        html+="<div class=\"interests\">Interests:<ul type = \"disc\">";
+        html += "<div id=\"sc-edprofile\">";
+        html += '<div alight="center" style="font-size:25px">Edit Profile Form</div>';
+        html += '<form name="userEditForm" action="test.php" method="POST">';
+        html += '<input type="text" size="10" name="first_name" value="' + data.first_name + '"/>'
+        html += '<input type="text" name="last_name" value="' + data.last_name + '"/>'
+        html += '<input type="number"  name="age" value="' + data.age + '"/>'
+        html += '<input type="text" name="phone" value="' + data.phone + '"/>'
+        html += "<textarea value=\"Add interest\" /></textarea>";
+        html += "<button id=\"addButton\">Add</button>";
+        html += `<select>
+        <option value="" selected="selected" >` + status + `</option>
+        <option value="1">Acive</option>
+        <option value="2">Non Active</option>
+      </select>`;
+        html += "<div class=\"interests\">Interests:<ul type = \"disc\">";
         for(var i = 0; i < (data.interests.length); i++){
-        html+="<li align=\"left\" style=\"font-size:20px\">" + data.interests[i].description + "</li>";
+        html += '<li align="left" style="font-size:20px">' + data.interests[i].description + "</li>";
         }
-        html+="</ul></div>";
-        console.log(html);
-        console.log($('div.content'));
+        html += "</ul></div>";
+        html += "<input type=\"submit\" value=\"Save\" />";
+        html += "</form>";
+        html += "</div>";
+        console.log(html);  
         $('div.content').html(html);
         });
   });
